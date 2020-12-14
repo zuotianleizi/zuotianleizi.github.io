35f049a3fb4f1935adb04c889eb8e2d14172a398
zuotianleizi <zuotianleizi.foxmail.com>
2020-12-14 13:07:55 +0800

    Site updated: 2020-12-14 13:07:54

--- js/chocolate.js
+++ js/chocolate.js
@@ -6,3 +6,22 @@ var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: tran
 document.getElementById("go-up").innerHTML += top_up;
     })
     
+    // 可爱的Title
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function() {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "/img/favicon.ico");
+        document.title = '(つェ⊂) 我藏好了哦~~';
+        clearTimeout(titleTime);
+    } else {
+        $('[rel="icon"]').attr('href', "/img/favicon.ico");
+        document.title = '(*´∇｀*) 被你发现啦~~' + OriginTitle;
+        titleTime = setTimeout(function() {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
+
+var app = document.getElementById('footer');
+app.removeAttribute('id')
